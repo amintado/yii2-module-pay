3b0fa45b63dcefa38dfb53c4346159dc82e29a1b
غلامرضا بهشتیان <amintado@gmail.com>
2017-10-12 18:59:32 +0330

    start develope

---
+++ Module.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: amin__000
+ * Date: 10/12/2017
+ * Time: 5:58 PM
+ */
+
+namespace vendor\amintado\pay;
+
+
+class Module extends \yii\base\Module
+{
+    public $api='';
+    public $controllerNamespace='amintado\pay\controllers';
+}
---
+++ controllers/DefaultController.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: amin__000
+ * Date: 10/12/2017
+ * Time: 6:58 PM
+ */
+
+namespace amintado\pay\controllers;
+
+
+use yii\web\Controller;
+
+class DefaultController extends Controller
+{
+    public function actionindex()
+    {
+        return 'start';
+    }
+}
